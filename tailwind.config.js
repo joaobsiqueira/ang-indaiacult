c251d0c9de678d2ea45a38cc008601657d5cca51
jsiqueira <usernamejoaozinho@gmail.com>
2023-11-13 17:43:09 -0300

    feat(added navbar): added navbar prototype
    the aesthetic part of the navbar is complete, but it is not interactive

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    colors: {
+      MainHotWhite: "#F87060",
+      MainColdWhite: "#3E8989",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
